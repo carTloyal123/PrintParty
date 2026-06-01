e09cc984ed3d17374cdb628c25644f7783f0e36b
Carson Loyal <carsonloyal123@me.com>
2026-05-31 22:10:08 -0500

    Initial Commit

---
+++ PrintParty/PrintPartyApp.swift
@@ -0,0 +1,32 @@
+//
+//  PrintPartyApp.swift
+//  PrintParty
+//
+//  Created by Carson Loyal on 5/31/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PrintPartyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
